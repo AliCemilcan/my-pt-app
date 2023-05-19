101922e7507d5a9de05d5306d5dcc252cbbc79e7
Ali Cemilcan Ciftarslan <ciftarsl@sheridancollege.ca>
2023-05-19 18:06:25 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignIn from './authorization/SignIn'
+import SignUp from './authorization/SingUp'
+
 import './App.css'
 
 function App() {
@@ -8,6 +12,11 @@ function App() {
 
   return (
     <>
+      <BrowserRouter>
+         <Routes>
+            <Route path="/singin" element={<SignIn />} />
+            <Route path="/singup" element={<SignUp />} />
+          </Routes>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +37,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      </BrowserRouter>
     </>
   )
 }
